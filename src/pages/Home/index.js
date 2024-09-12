eaa9c8174781266ed823a7a0b3603655eb4b4d9d
Victor <paulo9.victor.silva@gmail.com>
2024-09-12 16:22:59 -0300

    fix: signout function

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
-import useAuth from "../../hooks/useAuth";
 import * as C from "./styles";
 
 const Home = () => {
-  const { signout } = useAuth();
   const navigate = useNavigate();
+
+  function signout() {
+    localStorage.removeItem('token');
+  };
 
   return (
     <C.Container>
       <C.Title>Home</C.Title>
-      <Button Text="Sair" onClick={() => [signout(), navigate("/")]}>
+      <Button Text="Sair" onClick={() => [signout(), navigate("/signin")]}>
         Sair
       </Button>
     </C.Container>
