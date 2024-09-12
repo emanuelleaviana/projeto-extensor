5a6e851347f8aced246ef08c44ed5278a48eeb7a
Victor <paulo9.victor.silva@gmail.com>
2024-09-12 15:20:30 -0300

    refactor: signin page

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import * as C from "./styles";
 
-const Input = ({ type, placeholder, value, onChange }) => {
+const Input = ({ type, placeholder, value, name, onChange }) => {
   return (
     <C.Input
       value={value}
+      name={name}
       onChange={onChange}
       type={type}
       placeholder={placeholder}
--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,32 +1,31 @@
 import React, { useState } from "react";
-import Input from "../../components/Input";
-import Button from "../../components/Button";
-import * as C from "./styles";
 import { Link, useNavigate } from "react-router-dom";
-import useAuth from "../../hooks/useAuth";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import * as C from "./styles";
+import axios from "axios";
 
 const Signin = () => {
-  const { signin } = useAuth();
+  const [form, setForm] = useState({
+    registration: '',
+    password: '',
+  });
+
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
-  const [error, setError] = useState("");
+  function handleChange(event) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  }
+  
+  async function handleLogin() {
+    if(form.registration.length > 0 && form.password.length > 0) {
+      const response = await axios.post('https://nodejs-calcados-api-production.up.railway.app/api/auth/login/', form);
+      console.log(response.data);
 
-  const handleLogin = () => {
-    if (!email | !senha) {
-      setError("Preencha todos os campos");
-      return;
+      if(response.data.token) {
+        navigate('/home');
+      }
     }
-
-    const res = signin(email, senha);
-
-    if (res) {
-      setError(res);
-      return;
-    }
-
-    navigate("/home");
   };
 
   return (
@@ -34,18 +33,19 @@ const Signin = () => {
       <C.Label>SISTEMA DE LOGIN</C.Label>
       <C.Content>
         <Input
-          type="email"
-          placeholder="Digite seu E-mail"
-          value={email}
-          onChange={(e) => [setEmail(e.target.value), setError("")]}
+          type="text"
+          name="registration"
+          placeholder="Digite sua Matrícula"
+          onChange={handleChange}
+          value={form.registration}
         />
         <Input
           type="password"
+          name="password"
           placeholder="Digite sua Senha"
-          value={senha}
-          onChange={(e) => [setSenha(e.target.value), setError("")]}
+          onChange={handleChange}
+          value={form.password}
         />
-        <C.labelError>{error}</C.labelError>
         <Button Text="Entrar" onClick={handleLogin} />
         <C.LabelSignup>
           Não tem uma conta?
